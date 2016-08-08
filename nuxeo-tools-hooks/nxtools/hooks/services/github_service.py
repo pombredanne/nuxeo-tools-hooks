pombredanne/nuxeo-tools-hooks

commit 62960e4936eac15147fe5a61250527fa3720321b

NXBT-1191: Cleanup other empty lines from sync logs

nuxeo-tools-hooks/nxtools/hooks/services/github_service.py
@@ -183,7 +183,7 @@ class GithubService(AbstractService):
         try:
             opened_pulls = repository.get_pulls()
 
-            if opened_pulls:
+            if opened_pulls.totalCount:
                 log.info('Updating %s/%s pull requests: %s',
                          organization_name, repository_name, ", ".join([str(pull.number) for pull in opened_pulls]))
 

nuxeo-tools-hooks/nxtools/hooks/tests/test_github_service.py
@@ -28,6 +28,12 @@ from nxtools.hooks.tests import HooksTestCase
 
 class GithubServiceTest(HooksTestCase):
 
+    class MockedPullRequestList(list):
+
+        @property
+        def totalCount(self):
+            return self.__len__()
+
     def setUp(self):
         super(GithubServiceTest, self).setUp()
 
@@ -49,10 +55,13 @@ class GithubServiceTest(HooksTestCase):
             }
         }
 
+        pull_requests_list = GithubServiceTest.MockedPullRequestList()
+        pull_requests_list.append(PullRequest(None, {}, pull_request, True))
+
         self.assertEqual(0, len(StoredPullRequest.objects))
 
         self.mocks.repository.name = 'mock_repository'
-        self.mocks.repository.get_pulls.return_value = [PullRequest(None, {}, pull_request, True)]
+        self.mocks.repository.get_pulls.return_value = pull_requests_list
         self.mocks.repository.organization = self.mocks.organization
         self.mocks.organization.get_repos.return_value = [self.mocks.repository]
         services.get(Config).set_request_environ({
@@ -78,7 +87,10 @@ class GithubServiceTest(HooksTestCase):
         new_pull_request['number'] = 43
         pull_request['state'] = "closed"
 
-        self.mocks.repository.get_pulls.return_value = [PullRequest(None, {}, new_pull_request, True)]
+        pull_requests_list = GithubServiceTest.MockedPullRequestList()
+        pull_requests_list.append(PullRequest(None, {}, new_pull_request, True))
+
+        self.mocks.repository.get_pulls.return_value = pull_requests_list
         self.mocks.repository.get_pull.return_value = PullRequest(None, {}, pull_request, True)
 
         github.sync_pull_requests()

commit 8b678af9351594e709d776f952614b4bdabba435

NXBT-1211: Fix regression on PR sync introduced with 62960e4936eac15147fe5a61250527fa3720321b (NXBT-1191)

nuxeo-tools-hooks/nxtools/hooks/services/github_service.py
@@ -182,8 +182,13 @@ class GithubService(AbstractService):
         log.info('Syncing pull requests of %s/%s', organization_name, repository_name)
         try:
             opened_pulls = repository.get_pulls()
+            pulls_count = 0
 
-            if opened_pulls.totalCount:
+            # Fix no count available on pulls list
+            for _ in opened_pulls:
+                pulls_count += 1
+
+            if pulls_count > 0:
                 log.info('Updating %s/%s pull requests: %s',
                          organization_name, repository_name, ", ".join([str(pull.number) for pull in opened_pulls]))
 